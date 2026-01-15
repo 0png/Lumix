56cd8fe79e5ea73d96a455c6a894a7725e96e8bf
0png <blackwolfyt0708@gmail.com>
2026-01-15 16:43:41 +0800

    fix: exclude uuid from externalizeDepsPlugin to fix ESM import error

--- packages/app/electron.vite.config.ts
+++ packages/app/electron.vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [externalizeDepsPlugin({ exclude: ['uuid'] })],
     build: {
       rollupOptions: {
         input: {
