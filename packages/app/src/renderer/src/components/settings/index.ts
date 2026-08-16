eae0503b723114079856c03400ffe4c96e261496
0png <blackwolfyt0708@gmail.com>
2026-08-16 22:12:21 +0800

    chore(repo): remove stale tracked artifacts

--- packages/app/src/renderer/src/components/settings/index.ts
+++ packages/app/src/renderer/src/components/settings/index.ts
@@ -1,4 +1,2 @@
 export { SettingsDialog } from './SettingsDialog';
-export { AboutDialog } from './AboutDialog';
-export { SettingsView } from './SettingsView';
 export { AboutView } from './AboutView';
